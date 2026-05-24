f82934c84270bb79eca930b94ea50ebb67e231b0
leorechenberg <leorechenberg@gmail.com>
2026-05-23 23:39:55 -0300

    Add files via upload

---
+++ Backups-codigos/Jogo_V4_Leonardo/Gerenciadores.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+class Ente;
+
+namespace Personagens {
+	class Jogador;
+}
+using namespace Personagens;
+
+namespace Gerenciadores {
+
+	class GerenciadorGrafico {
+	private:
+		sf::RenderWindow obj;
+
+	public:
+		GerenciadorGrafico();
+		~GerenciadorGrafico();
+
+		void desenharEnte(Ente* pE);
+		void desenharFundo();
+		bool isOpen();
+		void atualizarEventos();
+		void clear();
+		void display();
+		sf::RenderWindow& getJanela() const;
+	};
+
+	class GerenciadorColisoes {
+	private:
+		Jogador* pJog1;
+
+	public:
+		GerenciadorColisoes();
+		~GerenciadorColisoes();
+
+		void setJog1(Jogador* pJg1);
+		void tocandoChao();
+	};
+
+}
---
+++ Backups-codigos/Jogo_V4_Leonardo/Principal.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Gerenciadores.h"
+#include "stdio.h"
+#include "Personagens.h"
+
+using namespace Personagens;
+
+class Jogo {
+private:
+
+	Jogador* pJog1;
+	Jogador* pJog2;
+	Gerenciadores::GerenciadorGrafico GG;
+	Gerenciadores::GerenciadorColisoes GC;
+
+public:
+	Jogo();
+	~Jogo();
+
+	Jogador* getJogador1();
+	void executar();
+};
