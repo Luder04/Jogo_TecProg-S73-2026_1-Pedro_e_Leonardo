a338bdd89a1f80548a5f839997f87db8e1daa915
Luder04 <ludergamespmm@gmail.com>
2026-05-25 21:34:30 -0300

    Add files via upload

---
+++ Versoes-codigos/Jogo V6 Pedro/Gerenciadores.cpp
@@ -0,0 +1,84 @@
+#include "Gerenciadores.h"
+#include <iostream>
+#include "Ente.h"
+#include "Personagens.h"
+
+using namespace std;
+using namespace Gerenciadores;
+
+//GRAFICO
+GerenciadorGrafico::GerenciadorGrafico() : obj(sf::VideoMode({ 1280, 720 }), "Jogo") {
+	sf::View view;
+	view.setCenter(sf::Vector2f(640.f, 360.f));
+	view.setSize(sf::Vector2f(1280.f, -720.f));
+	obj.setView(view);
+
+	obj.setFramerateLimit(60);
+}
+GerenciadorGrafico::~GerenciadorGrafico() { obj.close(); }
+void GerenciadorGrafico::desenharEnte(Ente* pE) {
+	obj.draw(pE->getFig());
+}
+void GerenciadorGrafico::desenharFundo(int corCeuHex, int corChaoHex) {
+	sf::RectangleShape ceu;
+	ceu.setFillColor(sf::Color(corCeuHex));
+	//ceu.setFillColor(sf::Color(104, 173, 237));
+	ceu.setPosition(0, 200);
+	ceu.setSize({ 1280, 520 });
+	obj.draw(ceu);
+
+	sf::RectangleShape chao;
+	chao.setFillColor(sf::Color(corChaoHex));
+	//chao.setFillColor(sf::Color(143, 123, 93));
+	chao.setPosition(0, 0);
+	chao.setSize({ 1280, 200 });
+	obj.draw(chao);
+}
+bool GerenciadorGrafico::isOpen() {
+	return obj.isOpen();
+}
+void GerenciadorGrafico::atualizarEventos() {
+	sf::Event evento;
+	while (obj.pollEvent(evento)) {
+		if (evento.type == sf::Event::Closed) {
+			obj.close();
+		}
+	}
+}
+void GerenciadorGrafico::clear() {
+	obj.clear();
+}
+void GerenciadorGrafico::display() {
+	obj.display();
+}
+sf::RenderWindow& GerenciadorGrafico::getJanela() const {
+	return const_cast<sf::RenderWindow&>(obj);
+}
+
+//COLISOES
+GerenciadorColisoes::GerenciadorColisoes() : pJog1(NULL), LI() {
+
+}
+GerenciadorColisoes::~GerenciadorColisoes() {}
+void GerenciadorColisoes::setJog1(Jogador* pJg1) {
+	if (pJg1) {
+		pJog1 = pJg1;
+	}
+}
+void GerenciadorColisoes::tocandoChao() {
+	if (pJog1->gety() + pJog1->vely < 200 - 50) {
+		pJog1->podePular = true;
+		pJog1->vely = 0;
+		pJog1->setPosition(pJog1->getx(), 200);
+	}
+}
+
+void GerenciadorColisoes::incluirInimigo(Inimigo *pI) {
+	if (pI) {
+		LI.push_back(pI);
+	}
+}
+
+int GerenciadorColisoes::getQtdInimigosFacies() {
+	return (int)LI.size();
+}
---
+++ Versoes-codigos/Jogo V6 Pedro/Principal.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Gerenciadores.h"
+#include "stdio.h"
+#include "Personagens.h"
+
+using namespace Personagens;
+
+class Jogo {
+private:
+
+	Jogador* pJog1;
+	Jogador* pJog2;
+	Gerenciadores::GerenciadorGrafico GG;
+	Gerenciadores::GerenciadorColisoes GC;
+public:
+	int proxFase;
+
+public:
+	Jogo();
+	~Jogo();
+
+	Jogador* getJogador1();
+	void executar();
+};
