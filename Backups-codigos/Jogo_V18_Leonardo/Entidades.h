853db6cd666e1b83bcea9228ddedb2b2570d9e8a
leorechenberg <leorechenberg@gmail.com>
2026-06-07 03:18:43 -0300

    Add files via upload

---
+++ Backups-codigos/Jogo_V18_Leonardo/Entidades.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "Ente.h"
+#include <iostream>
+
+namespace Entidades {
+
+	class Entidade : public Ente {
+	protected:
+		float x;
+		float y;
+
+		float sizex;
+		float sizey;
+
+        float vely;
+		float velx;
+
+		ostream buffer;
+		bool ehObstaculo;
+
+		static int fase;
+		static const float gravidade;
+
+	public:
+		Entidade();
+		virtual ~Entidade();
+
+		virtual void executar() = 0;
+		virtual void salvar() = 0;
+		float getx();
+		float gety();
+		float getSizex();
+		float getSizey();
+		float getvelx();
+		float getvely();
+		void setvelx(float x);
+		void setvely(float y);
+		void setPosition(float x, float y);
+		virtual const int getVidas() = 0;
+		const bool getObs();
+		ostream& getBuffer();
+		std::ostream& getConteudoBuffer();
+
+		static void setFase(int f);
+
+	protected:
+		void salvarDataBuffer();
+		void setBuffer(std::streambuf* sb);
+
+	};
+
+    class Projetil : public Entidade{
+    protected:
+        bool ativo;
+
+    public:
+        Projetil();
+        ~Projetil();
+
+        void executar();
+        void salvar();
+        const int getVidas();
+    };
+}
