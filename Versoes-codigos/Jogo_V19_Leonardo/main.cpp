74c16c20ac16a9fa88e33a80ec72c203bde71105
leorechenberg <leorechenberg@gmail.com>
2026-06-07 23:11:29 -0300

    Add files via upload

---
+++ Versoes-codigos/Jogo_V19_Leonardo/main.cpp
@@ -0,0 +1,14 @@
+//Versao 19
+//Terminei de implementar chefões e projéteis.
+//Para vencer o jogo, o jogador deve matar os 3 chefões e chegar no fim da fase 2
+//Ao vencer o jogo, volta ao menu e aparece sua pontuação
+
+#include "Principal.h"
+
+int main()
+{
+    Jogo jogo;
+    jogo.executar();
+
+    return 0;
+}
