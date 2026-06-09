223875400356dcb6dfa014cc0f91b47ac494298a
leorechenberg <leorechenberg@gmail.com>
2026-06-08 21:30:03 -0300

    Add files via upload

---
+++ Backups-codigos/Jogo_V20_Leonardo/main.cpp
@@ -0,0 +1,13 @@
+//Versao 20
+//corrigido bug que fazia crashar na fase 2 (eu tinha esquecido de colocar para limpar a lista de projéteis)
+//começo da implementação do ranking (por enquanto tem que digitar o nome pela janela de console)
+
+#include "Principal.h"
+
+int main()
+{
+    Jogo jogo;
+    jogo.executar();
+
+    return 0;
+}
