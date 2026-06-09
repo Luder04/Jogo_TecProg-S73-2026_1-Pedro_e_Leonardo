2b3fe6573926dc2b241319e844675895302bb0b3
leorechenberg <leorechenberg@gmail.com>
2026-06-08 21:29:11 -0300

    Add files via upload

---
+++ Versoes-codigos/Jogo_V20_Leonardo/Fases.cpp
@@ -0,0 +1,235 @@
+#include "Fases.h"
+#include "Principal.h"
+#include "Obstaculos.h"
+
+using namespace Obstaculos;
+using namespace Fases;
+
+//FASE ABSTRATA
+Fase::Fase() : Ente(), pJog1(NULL), pJogo(NULL), lista_ents(), GC() {
+    cout << "Fase de Id: " << getId() << " criado no Jogo" << endl;
+}
+Fase::~Fase() {
+}
+void Fase::salvarEntidades(std::ofstream& arquivo) {
+    lista_ents.salvar(arquivo);
+}
+void Fase::setJogadores(Jogador* pJog1_in/*,Jogador* pJog2_in*/) {
+    if (pJog1_in != NULL) {
+        pJog1 = pJog1_in;
+        //lista_ents.setJogador(pJog1_in);
+        GC.setJog1(pJog1);
+
+        lista_ents.incluir(static_cast<Entidade*>(pJog1));
+
+    }
+
+    /*if(pJog2_in!=NULL){
+        pJog2=pJog2_in;
+        GC.setJog2(pJog2_in);
+    }*/
+}
+void Fase::setJogo(Jogo* j) {
+    pJogo = j;
+}
+void Fase::executar() {
+    //comandos comuns a ambas as fases:
+    lista_ents.percorrer();
+
+
+    GC.tocandoChao();
+    GC.tratarColisoesJogsInimigos();
+    GC.tratarColisoesJogsObstaculo();
+    GC.tratarColisoesInimObstaculo();
+    GC.tratarColisoesProjObstaculo();
+    GC.tratarColisoesJogsProjeteis();
+    GC.tratarColisoesProjChao();
+
+    lista_ents.desenhar(); //desenhar voltar a estar aqui
+
+    Ente::pGG->display();
+
+    //se morreu ou apertou esc, volta para o menu:
+    if (pJog1->getVidas()<1 || sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)){
+
+            if(pJog1->getVidas()<1){
+                cout<<"Jogador perdeu todas as vidas! Voltando para o menu..."<<endl;
+                pJogo->menu_principal.gameOver();
+            } else {
+                cout<<"Jogador saiu do jogo! Voltando para o menu..."<<endl;
+            }
+            pJog1->reset();
+            pJogo->proxFase = 0;
+
+            limpar();
+    }
+}
+void Fase::criarInimFaceis() {
+    int max_qtd_inim_facil = 5;//3 + rand() % 8;
+    int i;
+    Goomba* inim = NULL;
+
+    for (i = 0; i < max_qtd_inim_facil; i++) {
+        inim = new Goomba();
+        lista_ents.incluir(static_cast<Entidade*>(inim));
+        GC.incluirInimigo(static_cast<Inimigo*>(inim));
+
+    }
+
+}
+void Fase::criarPlataformas() {
+    int max = 6;
+
+    for (int i = 0; i < max; i++) {
+        Plataforma* plat = new Plataforma();
+        GC.incluirObstaculo(static_cast<Obstaculo*>(plat));
+        lista_ents.incluir(static_cast<Entidade*>(plat));
+    }
+
+}
+void Fase::criarCenario() {
+
+}
+void Fase::limpar() {
+    lista_ents.limpar();
+    GC.limparListas();
+}
+
+//FASE PRIMEIRA
+FasePrimeira::FasePrimeira() : maxInimMedios(3) {
+
+}
+FasePrimeira::~FasePrimeira() {
+
+}
+void FasePrimeira::executar() {
+    pJog1->setPosition(100, 200);
+    criarObstaculo();
+    criarInimigos();
+
+
+    while (Ente::pGG->isOpen() && pJogo->proxFase == 1) {
+        Ente::pGG->atualizarEventos();
+        Ente::pGG->clear();
+        Ente::pGG->desenharFundo(0x68ADEDFF, 0x8F7B5DFF);//a cor é definida pelo código hexadecimal, escrever na forma: 0x"Codigo"FF
+        Fase::pGG->desenharVidas(pJog1);
+
+        Fase::executar();
+
+        //código para passar para a fase 2: (provisoriamente é só ir para o canto direito do cenário)
+        if (pJog1->getx() > 1250) {
+            pJogo->proxFase = 2;
+            limpar();
+            return;
+        }
+    }
+}
+
+void FasePrimeira::criarInimigos() {
+    criarInimFaceis();
+    criarInimMedios();
+}
+void FasePrimeira::criarObstaculo() {
+    criarPlataformas();
+    criarObstMedios();
+}
+void FasePrimeira::criarInimMedios() {
+    int i;
+    Inim_Medio* inim = NULL;
+
+    for (i = 0; i < maxInimMedios; i++) {
+        inim = new Inim_Medio();
+        lista_ents.incluir(static_cast<Entidade*>(inim));
+        GC.incluirInimigo(static_cast<Inimigo*>(inim));
+    }
+}
+void FasePrimeira::criarObstMedios() {
+    int max = 4;
+
+    for (int i = 0; i < max; i++) {
+        Obst_Medio* lama = new Obst_Medio();
+        GC.incluirObstaculo(static_cast<Obstaculo*>(lama));
+        lista_ents.incluir(static_cast<Entidade*>(lama));
+    }
+
+}
+
+
+//FASE SEGUNDA
+FaseSegunda::FaseSegunda() : maxChefoes(3) {
+
+}
+FaseSegunda::~FaseSegunda() {
+
+}
+void FaseSegunda::executar() {
+    pJog1->setPosition(100, 200);
+    criarInimigos();
+    criarObstaculo();
+
+    while (Ente::pGG->isOpen() && pJogo->proxFase == 2) {
+        Ente::pGG->atualizarEventos();
+        Ente::pGG->clear();
+        Ente::pGG->desenharFundo(0xC1B891FF, 0xB5785AFF);//EFE4B0 B97A57
+        Fase::pGG->desenharVidas(pJog1);
+        for(int i=0;i<lista_chefoes.size();i++){
+            if(lista_chefoes[i]!=NULL){
+                if (lista_chefoes[i]->getAtirar()==true){
+                    x_projetil=lista_chefoes[i]->getx();
+                    y_projetil=lista_chefoes[i]->gety()+lista_chefoes[i]->getSizey();
+                    criarProjeteis();
+                }
+                if (lista_chefoes[i]->getVidas()==0){
+                    lista_chefoes.erase(lista_chefoes.begin() + i);
+                }
+            }
+        }
+        Fase::executar();
+        //código para completar o jogo fase 2: (provisoriamente é só ir para o canto direito do cenário)
+        if (pJog1->getx() > 1250 && (lista_chefoes.empty())) {
+            pJogo->menu_principal.vitoria(pJog1->getPontos()+pJog1->getVidas());
+            pJog1->reset();
+            pJogo->proxFase = 0;
+            limpar();
+        }
+    }
+}
+
+void FaseSegunda::criarInimigos() {
+    criarInimFaceis();
+    criarChefoes();
+}
+void FaseSegunda::criarObstaculo() {
+    criarPlataformas();
+    criarObstDificeis();
+}
+void FaseSegunda::criarChefoes() {
+    int i;
+    Chefao* inim = NULL;
+
+    for (i = 0; i < maxChefoes; i++) {
+        inim = new Chefao();
+        lista_ents.incluir(static_cast<Entidade*>(inim));
+        GC.incluirInimigo(static_cast<Inimigo*>(inim));
+        lista_chefoes.push_back(inim);
+    }
+}
+void FaseSegunda::criarObstDificeis() {
+    int max = 5;
+    for (int i = 0; i < max; i++) {
+        Obst_Dificil* lava = new Obst_Dificil();
+        GC.incluirObstaculo(static_cast<Obstaculo*>(lava));
+        lista_ents.incluir(static_cast<Entidade*>(lava));
+    }
+
+}
+void FaseSegunda::criarProjeteis() {
+    Projetil* pj = new Projetil(x_projetil,y_projetil);
+    GC.incluirProjetil(pj);
+    lista_ents.incluir(static_cast<Entidade*>(pj));
+}
+
+void FaseSegunda::limpar() {
+    lista_chefoes.clear();
+    Fase::limpar();
+}
---
+++ Versoes-codigos/Jogo_V20_Leonardo/Obstaculos.cpp
@@ -0,0 +1,220 @@
+#include "Obstaculos.h"
+#include "Personagens.h"
+
+using namespace Personagens;
+using namespace Obstaculos;
+
+// Classe abstrata
+Obstaculo::Obstaculo() : Entidades::Entidade(), danoso(false) {
+    ehObstaculo = true;
+}
+Obstaculo::~Obstaculo() { danoso = false;  }
+void Obstaculo::salvarDataBuffer() {
+	Entidade::salvarDataBuffer();
+}
+const int Obstaculo::getVidas() { return 0; } // tratar depois
+
+// Plataforma
+int Plataforma::qtd_plats = 0;
+
+Plataforma::Plataforma() : Obstaculo(), altura(0), largura(0) {
+	qtd_plats++;
+
+    x = rand() % 60 + (200.0f * qtd_plats);
+    y=200;
+    if(rand()%2 == 0){
+        //plataforma horizontal alta
+        y += 30 + rand() % 25;
+        altura = rand()%25 + 25.0f;
+        largura = rand()%70 + 50.0f;
+    } else{
+        //plataforma vertical no chão
+        altura = rand()%30 + 80.0f;
+        largura = rand()%30 + 30.0f;
+    }
+
+    sf::RectangleShape* shape = new sf::RectangleShape(sf::Vector2f(largura, altura));
+    shape->setFillColor(sf::Color(0xEFEFEFFF));
+
+	shape->setPosition(x, y);
+
+	pFig = shape;
+}
+
+Plataforma::~Plataforma() {
+	altura = 0.0f;
+	qtd_plats = 0;
+}
+
+void Plataforma::salvar() {
+	Obstaculo::salvarDataBuffer();
+}
+void Plataforma::executar() {
+    //desenhar();
+}
+void Plataforma::obstaculizar(Jogador* pJog) {
+    sf::FloatRect boundsObs = (this)->getFig().getGlobalBounds();
+    sf::FloatRect boundsJogador = pJog->getFig().getGlobalBounds();
+
+    if (!boundsJogador.intersects(boundsObs)) return;
+
+    // Colisão vertical
+    if (boundsJogador.left + boundsJogador.width > boundsObs.left + 5.0f &&
+        boundsJogador.left < boundsObs.left + boundsObs.width - 5.0f) {
+
+        // De cima para baixo
+        if (pJog->getvely() <= 0.0f) {
+            float topoObs = boundsObs.top + boundsObs.height;
+            pJog->setPosition(pJog->getx(), topoObs);
+            pJog->setvely(0);
+            pJog->podePular = true;
+        }
+        // De baixo para cima
+        else if (pJog->getvely() > 0.0f) {
+            float fundoObs = boundsObs.top - boundsJogador.height;
+            pJog->setPosition(pJog->getx(), fundoObs);
+            pJog->setvely(-1.0f); // Para a subida e começa a cair
+        }
+    }
+
+    // Colisão lateral
+    else {
+        if (boundsJogador.left + boundsJogador.width / 2.0f < boundsObs.left + boundsObs.width / 2.0f) {
+            pJog->setPosition(boundsObs.left - boundsJogador.width, pJog->gety());
+        }
+        else {
+            pJog->setPosition(boundsObs.left + boundsObs.width, pJog->gety());
+        }
+        pJog->setvelx(0.0f);
+    }
+}
+void Plataforma::obstaculizarInimigo(Inimigo* pInim){
+    sf::FloatRect boundsObs = (this)->getFig().getGlobalBounds();
+    sf::FloatRect boundsInimigo = pInim->getFig().getGlobalBounds();
+
+    if (!boundsInimigo.intersects(boundsObs)) return;
+
+    // Colisão vertical
+    if (boundsInimigo.left + boundsInimigo.width > boundsObs.left + 5.0f &&
+        boundsInimigo.left < boundsObs.left + boundsObs.width - 5.0f) {
+
+        // De cima para baixo
+        if (pInim->getvely() <= 0.0f) {
+            float topoObs = boundsObs.top + boundsObs.height;
+            pInim->setPosition(pInim->getx(), topoObs);
+            pInim->setvely(0);
+        }
+        // De baixo para cima
+        else if (pInim->getvely() > 0.0f) {
+            float fundoObs = boundsObs.top - boundsInimigo.height;
+            pInim->setPosition(pInim->getx(), fundoObs);
+            pInim->setvely(-1.0f); // Para a subida e começa a cair
+        }
+    }
+
+    // Colisão lateral
+    else {
+        if (boundsInimigo.left + boundsInimigo.width / 2.0f < boundsObs.left + boundsObs.width / 2.0f) {
+            pInim->setPosition(boundsObs.left - boundsInimigo.width, pInim->gety());
+        }
+        else {
+            pInim->setPosition(boundsObs.left + boundsObs.width, pInim->gety());
+        }
+        pInim->setvelx(-pInim->getVelx());
+    }
+}
+// Obstaculo Medio
+int Obst_Medio::qtd_obstMedio = 0;
+
+Obst_Medio::Obst_Medio() : Obstaculo(), largura(0) {
+	qtd_obstMedio++;
+
+    float altura=10;
+
+	x = 205*qtd_obstMedio + rand()%90;
+	y = 201-altura;
+	largura = rand()%30 + 60.0f;
+
+    sf::RectangleShape* shape = new sf::RectangleShape(sf::Vector2f(largura, altura));
+	shape->setFillColor(sf::Color(0x964B00FF));
+
+	shape->setPosition(x, y);
+
+	pFig = shape;
+}
+
+Obst_Medio::~Obst_Medio() {
+	largura = 0.0f;
+	qtd_obstMedio = 0;
+}
+
+void Obst_Medio::salvar() {
+	Obstaculo::salvarDataBuffer();
+}
+void Obst_Medio::executar() {
+    //desenhar();
+}
+void Obst_Medio::obstaculizar(Jogador* pJog) {
+    //pJog->podePular=false;
+    pJog->forca_pulo=0.4*pJog->forca_pulo;
+    pJog->forca_andar=0.4*pJog->forca_andar;
+}
+
+void Obst_Medio::obstaculizarInimigo(Inimigo* p){
+}
+
+
+// Obstaculo dificil
+int Obst_Dificil::qtd_obstDificil = 0;
+
+Obst_Dificil::Obst_Dificil() : Obstaculo(), danosidade(0), largura(0), temperatura(0), dT(2) {
+	qtd_obstDificil++;
+
+    float altura=10;
+    danosidade=rand()%100;
+
+	x = 205*qtd_obstDificil + rand()%90;
+	y = 201-altura;
+	largura = rand()%40 + 90.0f;
+
+    sf::RectangleShape* shape = new sf::RectangleShape(sf::Vector2f(largura, altura));
+	//shape->setFillColor(sf::Color(150+danosidade, 17, 17));  //0xF71111FF
+	shape->setPosition(x, y);
+
+	pFig = shape;
+}
+
+Obst_Dificil::~Obst_Dificil() {
+	largura = 0.0f;
+	danosidade=0;
+	qtd_obstDificil = 0;
+}
+
+void Obst_Dificil::salvar() {
+	Obstaculo::salvarDataBuffer();
+}
+void Obst_Dificil::executar() {
+    if(temperatura>100-danosidade){
+        danoso=true;
+        pFig->setFillColor(sf::Color(255, 17, 17));
+    } else {
+        danoso=false;
+        pFig->setFillColor(sf::Color(100, 17, 17));
+    }
+
+    temperatura+=dT;
+    if(temperatura>100 || temperatura<0){
+        dT=-dT;
+    }
+}
+void Obst_Dificil::obstaculizar(Jogador* pJog) {
+    if(danoso){
+        pJog->operator--();
+    }
+    //pJog->forca_pulo=0.8*pJog->forca_pulo;
+    pJog->forca_andar=0.8*pJog->forca_andar;
+}
+void Obst_Dificil::obstaculizarInimigo(Inimigo* p){
+}
+
+
