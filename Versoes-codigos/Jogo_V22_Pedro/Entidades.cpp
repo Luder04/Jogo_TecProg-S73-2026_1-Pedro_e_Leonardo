817c437e8d3e06e2180c25ba6785c13e9c6885ba
Luder04 <ludergamespmm@gmail.com>
2026-06-11 01:53:22 -0300

    Add files via upload

---
+++ Versoes-codigos/Jogo_V22_Pedro/Entidades.cpp
@@ -0,0 +1,101 @@
+#include "Entidades.h"
+#include <fstream>
+
+using namespace Entidades;
+
+const float Entidade::gravidade = 1;
+int Entidade::fase = 0;
+Entidade::Entidade() : Ente(), x(-1), y(-1), sizex(0), sizey(0), vely(0), velx(0), buffer(NULL), ehObstaculo(false) {
+
+}
+
+Entidade::~Entidade() {
+	x = -1;
+	y = -1;
+	sizex = 0;
+	sizey = 0;
+}
+
+void Entidade::setBuffer(std::streambuf* sb) {
+    buffer.rdbuf(sb);
+}
+
+void Entidade::salvarDataBuffer() {
+    buffer << "Fase:" << fase << " "
+        << typeid(*this).name() << " "
+        << x << " "
+        << y << " "
+        << sizex << " "
+        << sizey << "\n";
+}
+
+std::ostream& Entidade::getConteudoBuffer() {
+    return buffer;
+}
+
+ostream& Entidade::getBuffer() { return buffer; }
+float Entidade::getx() { return x; }
+float Entidade::gety() { return y; }
+float Entidade::getSizex() { return sizex; }
+float Entidade::getSizey() { return sizey; }
+float Entidade::getvelx() { return velx; }
+float Entidade::getvely() { return vely; }
+void Entidade::gravitar() {  
+    vely -= gravidade;
+    if (y + vely < 200) {
+        vely = 0;
+        setPosition(x, 200);
+    }
+}
+const bool Entidade::getObs() { return ehObstaculo; }
+void Entidade::setFase(int f) { fase = f; };
+void Entidade::setPosition(float xNew, float yNew) {
+	x = xNew;
+	y = yNew;
+	pFig->setPosition(xNew, yNew);
+}
+void Entidade::setvelx(float x){
+    velx=x;
+}
+void Entidade::setvely(float y){
+    vely=y;
+}
+
+
+//Projetil
+Projetil::Projetil() : Entidade(), ativo(false) {
+
+}
+
+Projetil::Projetil(float x_spawn, float y_spawn, Chefao* pChefao) : Entidade(), ativo(true), pChefe(pChefao) {
+    x=x_spawn;
+    y=y_spawn;
+    velx=-(5+rand()%5);
+    vely=10+rand()%10;
+    sf::CircleShape* shape = new sf::CircleShape(5);
+	shape->setFillColor(sf::Color(0x54270DFF));
+	shape->setPosition(x, y);
+	pFig = shape;
+}
+
+Projetil::~Projetil(){
+    ativo=false;
+}
+
+void Projetil::executar(){
+    x += velx;
+    y += vely;
+
+    gravitar();
+	pFig->move(velx, vely);
+}
+void Projetil::salvar(){
+
+}
+const int Projetil::getVidas() {
+    return (int)ativo;
+}
+
+void Projetil::setAtivo(bool a){
+    ativo=a;
+}
---
+++ Versoes-codigos/Jogo_V22_Pedro/Entidades.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "Ente.h"
+#include <iostream>
+
+namespace Personagens {
+	class Chefao;
+}
+
+namespace Entidades {
+
+	class Entidade : public Ente {
+	protected:
+		float x;
+		float y;
+
+		float sizex;
+		float sizey;
+
+        float vely;
+		float velx;
+
+		ostream buffer;
+		bool ehObstaculo;
+
+		static int fase;
+		static const float gravidade;
+
+	public:
+		Entidade();
+		virtual ~Entidade();
+
+		virtual void executar() = 0;
+		virtual void salvar() = 0;
+		float getx();
+		float gety();
+		float getSizex();
+		float getSizey();
+		float getvelx();
+		float getvely();
+		void setvelx(float x);
+		void setvely(float y);
+		void setPosition(float x, float y);
+		void gravitar();
+		virtual const int getVidas() = 0;
+		const bool getObs();
+		ostream& getBuffer();
+		std::ostream& getConteudoBuffer();
+
+		static void setFase(int f);
+
+	protected:
+		void salvarDataBuffer();
+		void setBuffer(std::streambuf* sb);
+
+	};
+
+    class Projetil : public Entidade{
+    protected:
+        bool ativo;
+
+	public:
+		Chefao* pChefe;
+
+    public:
+        Projetil();
+        Projetil(float x_spawn, float y_spawn, Chefao* pChefao);
+        ~Projetil();
+
+        void executar();
+        void salvar();
+        const int getVidas();
+        void setAtivo(bool a);
+    };
+}
