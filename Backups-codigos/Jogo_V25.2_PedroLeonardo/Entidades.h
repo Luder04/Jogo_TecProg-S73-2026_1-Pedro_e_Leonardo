024c2d903269e4bfc75ba6b5cbcf8b3946f1ce72
Luder04 <ludergamespmm@gmail.com>
2026-06-16 18:10:09 -0300

    Add files via upload

---
+++ Backups-codigos/Jogo_V25.2_PedroLeonardo/Entidades.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "Ente.h"
+#include <iostream>
+
+namespace Personagens {
+	class Dragao;
+}
+
+namespace Entidades {
+
+	class Entidade : public Ente {
+	protected:
+		float x;
+		float y;
+
+		float sizex;
+		float sizey;
+
+        float vely;
+		float velx;
+
+		ostream buffer;
+		bool ehObstaculo;
+
+		static int fase;
+		static const float gravidade;
+
+	public:
+		Entidade();
+		virtual ~Entidade();
+
+		virtual void executar() = 0;
+		virtual void salvar() = 0;
+		float getx();
+		float gety();
+		float getSizex();
+		float getSizey();
+		float getvelx();
+		float getvely();
+		void setvelx(float x);
+		void setvely(float y);
+		void setPosition(float x, float y);
+		void gravitar();
+		virtual const int getVidas() = 0;
+		const bool getObs();
+		ostream& getBuffer();
+		std::ostream& getConteudoBuffer();
+
+		static void setFase(int f);
+
+	protected:
+		void salvarDataBuffer();
+		void setBuffer(std::streambuf* sb);
+
+	};
+
+    class Projetil : public Entidade{
+    protected:
+        bool ativo;
+        Dragao* pDragao;
+
+    public:
+        Projetil();
+        Projetil(Dragao* pDragao);
+        ~Projetil();
+
+        void executar();
+        void salvar();
+        const int getVidas();
+        void desativar();
+		float arrasto();
+		Dragao* getDragao();
+    };
+}
---
+++ Backups-codigos/Jogo_V25.2_PedroLeonardo/main.cpp
@@ -0,0 +1,27 @@
+//Versão 25.1
+//adicionada textura única para o jogador 2, ajustada textura do jogador 1 (um pouco menos escura)
+//textura do jogo vira quando o jogador muda de sentido
+//lava fica escura quando não é danosa
+
+#include "Principal.h"
+
+int main()
+{
+    Jogo jogo;
+    jogo.executar();
+
+    return 0;
+}
+/*PARA FAZER:
+melhorar textura e hitbox da águia
+melhorar textura da lama?
+adicionar textura do chefão
+dar um nome para o chefão
+mudar a textura do menu. do jeito atual ele começa sem textura nenhuma, e quando sai de uma fase o menu fica com a textura daquela fase.
+
+mover checagem de posição pela função gravitar() para o GC?
+
+crash quando fica aberto por muito tempo?
+malloc: mismatching next->prev_size (unsorted)
+Aborted (core dumped)
+SAVES!*/
