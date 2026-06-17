4f52cb03b51a0010a3811ff3d5ce4370aa2f41a1
Luder04 <ludergamespmm@gmail.com>
2026-06-17 18:23:29 -0300

    Add files via upload

---
+++ Backups-codigos/Jogo_V27_LeonardoPedro/Entidades.cpp
@@ -0,0 +1,130 @@
+#include "Entidades.h"
+#include <fstream>
+#include "Personagens.h"
+
+using namespace Entidades;
+
+const float Entidade::gravidade = 1;
+int Entidade::fase = 0;
+Entidade::Entidade() : Ente(), x(-1), y(-1), sizex(0), sizey(0), vely(0), velx(0), buffer(NULL), ehObstaculo(false) {
+
+}
+
+Entidade::~Entidade() {
+	x = -1;
+	y = -1;
+	sizex = 0;
+	sizey = 0;
+}
+
+void Entidade::setBuffer(std::streambuf* sb) {
+    buffer.rdbuf(sb);
+}
+
+void Entidade::salvarDataBuffer() {
+    buffer << "Fase:" << fase << " "
+        << typeid(*this).name() << " "
+        << x << " "
+        << y << " "
+        << sizex << " "
+        << sizey << "\n";
+}
+
+std::ostream& Entidade::getConteudoBuffer() {
+    return buffer;
+}
+
+ostream& Entidade::getBuffer() { return buffer; }
+float Entidade::getx() { return x; }
+float Entidade::gety() { return y; }
+float Entidade::getSizex() { return sizex; }
+float Entidade::getSizey() { return sizey; }
+float Entidade::getvelx() { return velx; }
+float Entidade::getvely() { return vely; }
+void Entidade::gravitar() {
+    vely -= gravidade;
+    if (y + vely < 200) {
+        vely = 0;
+        setPosition(x, 200);
+    }
+}
+const bool Entidade::getObs() { return ehObstaculo; }
+void Entidade::setFase(int f) { fase = f; };
+void Entidade::setPosition(float xNew, float yNew) {
+	x = xNew;
+	y = yNew;
+	pFig->setPosition(xNew, yNew);
+}
+void Entidade::setvelx(float x){
+    velx=x;
+}
+void Entidade::setvely(float y){
+    vely=y;
+}
+
+
+//Projetil
+Projetil::Projetil() : Entidade(), pDragao(NULL), ativo(false) {
+
+}
+
+Projetil::Projetil(Dragao* pChefao) : Entidade(), ativo(true), pDragao(pChefao) {
+    x=pDragao->getx();
+    y=pDragao->gety()+pDragao->getSizey()/2;
+    velx=-(25+rand()%20);
+    vely=10+rand()%10;
+    sizex = 2.5;
+    sizey = 2.5;
+    sf::CircleShape* shape = new sf::CircleShape(sizex+ sizey);
+
+	shape->setFillColor(sf::Color(0x54270DFF));
+	shape->setPosition(x, y);
+	pFig = shape;
+	setTexture("bola.png");
+}
+
+Projetil::~Projetil(){
+    desativar();
+}
+
+void Projetil::executar(){
+    gravitar();
+    velx += arrasto();
+    x += velx;
+    y += vely;
+	pFig->move(velx, vely);
+}
+void Projetil::salvar(){
+
+}
+const int Projetil::getVidas() {
+    return (int)ativo;
+}
+
+void Projetil::desativar(){
+    if(pDragao!=NULL){
+        pDragao->removeProj();
+        pDragao=NULL;
+    }
+    ativo=false;
+}
+float Projetil::arrasto() {
+    float coeficiente = 0.02f;
+    float massaEspecifica = 0.0787f;
+    float area = sizex * sizey;
+    float forcaArrasto = (1.0f/ 2.0f) * coeficiente * massaEspecifica * area * (velx*velx);
+
+    if (velx > 0)
+        return -forcaArrasto;
+    else
+        return forcaArrasto;
+}
+Dragao* Projetil::getDragao(){
+    return pDragao;
+}
+
+void Projetil::setSprite() {
+	pSprite->setTexture(*pTexture);
+    pSprite->setScale(0.4, 0.4);
+    pSprite->setPosition(pFig->getPosition().x - sizex*0.20, pFig->getPosition().y - sizey*0.1);
+}
---
+++ Backups-codigos/Jogo_V27_LeonardoPedro/Obstaculos.cpp
@@ -0,0 +1,290 @@
+#include "Obstaculos.h"
+#include "Personagens.h"
+
+using namespace Personagens;
+using namespace Obstaculos;
+
+// Classe abstrata
+Obstaculo::Obstaculo() : Entidades::Entidade(), danoso(false) {
+    ehObstaculo = true;
+}
+Obstaculo::~Obstaculo() { danoso = false;  }
+void Obstaculo::salvarDataBuffer() {
+	Entidade::salvarDataBuffer();
+}
+const int Obstaculo::getVidas() { return 0; } // tratar depois
+
+void Obstaculo::setSprite() {
+    pSprite->setTexture(*pTexture);
+    pSprite->setPosition(pFig->getPosition().x, pFig->getPosition().y);
+
+    sf::Vector2u texSize = pTexture->getSize();
+
+    float scaleX = sizex / texSize.x;
+    float scaleY = sizey / texSize.y;
+
+    pSprite->setScale(scaleX, scaleY);
+}
+
+// Plataforma
+int Plataforma::qtd_plats = 0;
+
+Plataforma::Plataforma() : Obstaculo(), altura(0), largura(0) {
+	qtd_plats++;
+
+    x = rand() % 60 + (200.0f * qtd_plats);
+    y=200;
+    if(rand()%2 == 0){
+        //plataforma horizontal alta
+        y += 30 + rand() % 25;
+        altura = (rand()%25 + 30.0f);
+        largura = rand()%70 + 55.0f;
+    } else{
+        //plataforma vertical no chão
+        altura = rand()%30 + 85.0f;
+        largura = rand()%30 + 35.0f;
+    }
+
+    altura=altura-(int)altura%8; //altura e largura são múltiplos de 8, para a textura ficar alinhada
+    largura=largura-(int)largura%8;
+
+    sizex = largura;
+    sizey = altura;
+
+    sf::RectangleShape* shape = new sf::RectangleShape(sf::Vector2f(sizex, sizey));
+    shape->setFillColor(sf::Color(0xEFEFEFFF));
+
+	shape->setPosition(x, y);
+
+	pFig = shape;
+    setTexture("plataformamenor2.png");
+    pTexture->setRepeated(true);
+    setSprite();
+}
+
+Plataforma::~Plataforma() {
+	altura = 0.0f;
+	qtd_plats = 0;
+}
+
+void Plataforma::salvar() {
+	Obstaculo::salvarDataBuffer();
+}
+void Plataforma::executar() {
+    gravitar();
+    flutuacao();
+    pFig->setPosition(x, y);
+    y += vely;
+    //desenhar();
+}
+void Plataforma::obstaculizar(Jogador* pJog) {
+    sf::FloatRect boundsObs = (this)->getFig().getGlobalBounds();
+    sf::FloatRect boundsJogador = pJog->getFig().getGlobalBounds();
+
+    if (!boundsJogador.intersects(boundsObs)) return;
+
+    // Colisão vertical
+    if (boundsJogador.left + boundsJogador.width > boundsObs.left + 5.0f &&
+        boundsJogador.left < boundsObs.left + boundsObs.width - 5.0f) {
+
+        // De cima para baixo
+        if (pJog->getvely() <= 0.0f) {
+            float topoObs = boundsObs.top + boundsObs.height;
+            pJog->setPosition(pJog->getx(), topoObs);
+            pJog->setvely(0);
+            pJog->podePular = true;
+        }
+        // De baixo para cima
+        else if (pJog->getvely() > 0.0f) {
+            float fundoObs = boundsObs.top - boundsJogador.height;
+            pJog->setPosition(pJog->getx(), fundoObs);
+            pJog->setvely(-1.0f); // Para a subida e começa a cair
+        }
+    }
+
+    // Colisão lateral
+    else {
+        if (boundsJogador.left + boundsJogador.width / 2.0f < boundsObs.left + boundsObs.width / 2.0f) {
+            pJog->setPosition(boundsObs.left - boundsJogador.width, pJog->gety());
+        }
+        else {
+            pJog->setPosition(boundsObs.left + boundsObs.width, pJog->gety());
+        }
+        pJog->setvelx(0.0f);
+    }
+}
+void Plataforma::obstaculizar(Inimigo* pInim){
+    sf::FloatRect boundsObs = (this)->getFig().getGlobalBounds();
+    sf::FloatRect boundsInimigo = pInim->getFig().getGlobalBounds();
+
+    if (!boundsInimigo.intersects(boundsObs)) return;
+
+    // Colisão vertical
+    if (boundsInimigo.left + boundsInimigo.width > boundsObs.left + 5.0f &&
+        boundsInimigo.left < boundsObs.left + boundsObs.width - 5.0f) {
+
+        // De cima para baixo
+        if (pInim->getvely() <= 0.0f) {
+            float topoObs = boundsObs.top + boundsObs.height;
+            pInim->setPosition(pInim->getx(), topoObs);
+            pInim->setvely(0);
+        }
+        // De baixo para cima
+        else if (pInim->getvely() > 0.0f) {
+            float fundoObs = boundsObs.top - boundsInimigo.height;
+            pInim->setPosition(pInim->getx(), fundoObs);
+            pInim->setvely(-1.0f); // Para a subida e começa a cair
+        }
+    }
+
+    // Colisão lateral
+    else {
+        if (boundsInimigo.left + boundsInimigo.width / 2.0f < boundsObs.left + boundsObs.width / 2.0f) {
+            pInim->setPosition(boundsObs.left - boundsInimigo.width, pInim->gety());
+        }
+        else {
+            pInim->setPosition(boundsObs.left + boundsObs.width, pInim->gety());
+        }
+        pInim->setvelx(-pInim->getVelx());
+        if (pInim->direcao == true) pInim->direcao = false; else pInim->direcao = true;
+    }
+}
+void Plataforma::flutuacao() {
+    if (y < 350) {
+        vely += 1 + (rand()%10)/100; // Experimente outros valores!!!
+    }
+    else if (y > 200) {
+        vely -= 1.03;
+    }
+    else if (y <= 200) {
+        vely = 0;
+    }
+}
+
+void Plataforma::setSprite(){
+    pSprite->setTexture(*pTexture);
+	pSprite->setPosition(pFig->getPosition().x, pFig->getPosition().y);
+	pSprite->setTextureRect(sf::IntRect({ 0, 0 }, { getSizex(), getSizey() }));
+}
+
+// Obstaculo Medio
+int Lama::qtd_lamas = 0;
+
+Lama::Lama() : Obstaculo(), largura(0) {
+	qtd_lamas++;
+
+    sizey=10;
+
+	x = 205*qtd_lamas + rand()%90;
+	y = 201- sizey;
+	largura = rand()%30 + 60.0f;
+    sizex = largura;
+
+    sf::RectangleShape* shape = new sf::RectangleShape(sf::Vector2f(sizex, sizey));
+	shape->setFillColor(sf::Color(0x964B00FF));
+
+	shape->setPosition(x, y);
+
+	pFig = shape;
+    setTexture("lama2.png");
+    //setSprite();
+}
+
+Lama::~Lama() {
+	largura = 0.0f;
+	qtd_lamas = 0;
+}
+
+void Lama::salvar() {
+	Obstaculo::salvarDataBuffer();
+}
+void Lama::executar() {
+    gravitar();
+    pFig->setPosition(x, y - 9);
+    y += vely;
+    //desenhar();
+}
+void Lama::obstaculizar(Jogador* pJog) {
+    //pJog->podePular=false;
+    pJog->forca_pulo=0.4*pJog->forca_pulo;
+    pJog->forca_andar=0.4*pJog->forca_andar;
+}
+
+void Lama::obstaculizar(Inimigo* p){
+}
+
+
+// Obstaculo dificil
+int Lava::qtd_lavas = 0;
+
+Lava::Lava() : Obstaculo(), danosidade(0), largura(0), temperatura(0), dT(2) {
+	qtd_lavas++;
+
+    float altura=10;
+    danosidade=rand()%100;
+
+	x = 205*qtd_lavas + rand()%90;
+	y = 201-altura;
+	largura = rand()%40 + 85.0f;
+
+    sizex = largura;
+    sizey = altura;
+
+    sf::RectangleShape* shape = new sf::RectangleShape(sf::Vector2f(sizex, sizey));
+	//shape->setFillColor(sf::Color(150+danosidade, 17, 17));  //0xF71111FF
+	shape->setPosition(x, y);
+
+	pFig = shape;
+    setTexture("lavaonoff.png");
+    pTexture->setRepeated(true);
+    setSprite();
+}
+
+Lava::~Lava() {
+	largura = 0.0f;
+	danosidade=0;
+	qtd_lavas = 0;
+}
+
+void Lava::salvar() {
+	Obstaculo::salvarDataBuffer();
+}
+void Lava::executar() {
+    if(temperatura>100-danosidade){
+        danoso=true;
+        pFig->setFillColor(sf::Color(255, 17, 17));
+    } else {
+        danoso=false;
+        pFig->setFillColor(sf::Color(100, 17, 17));
+    }
+
+    temperatura+=dT;
+    if(temperatura>100 || temperatura<0){
+        dT=-dT;
+    }
+
+    gravitar();
+    pFig->setPosition(x, y - 9);
+    y += vely;
+}
+void Lava::obstaculizar(Jogador* pJog) {
+    if(danoso){
+        pJog->operator--(2+danosidade/20);
+    }
+    //pJog->forca_pulo=0.8*pJog->forca_pulo;
+    pJog->forca_andar=0.8*pJog->forca_andar;
+}
+void Lava::obstaculizar(Inimigo* p){
+}
+
+void Lava::setSprite(){
+    pSprite->setTexture(*pTexture);
+	pSprite->setPosition(pFig->getPosition().x, pFig->getPosition().y);
+
+	if (danoso) {
+        pSprite->setTextureRect(sf::IntRect({ 0, 0 }, { getSizex(), getSizey() })); //se danoso, pega a parte de cima da imagem de textura (a parte clara)
+	}
+	else {
+	    pSprite->setTextureRect(sf::IntRect({ 0, 16 }, { getSizex(), getSizey() }));//se não danoso, pega a parte de baixo (parte escura)
+	}
+}
