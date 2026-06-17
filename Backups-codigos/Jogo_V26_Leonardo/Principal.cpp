a4c9c03ab6fa3d4992af8e977c45b9c0f94c32b4
leorechenberg <leorechenberg@gmail.com>
2026-06-17 15:31:16 -0300

    Add files via upload

---
+++ Backups-codigos/Jogo_V26_Leonardo/Principal.cpp
@@ -0,0 +1,103 @@
+#include "Principal.h"
+
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+
+void Jogo::randomizar() {
+	srand(static_cast<unsigned int>(time(NULL)));
+}
+
+Jogo::Jogo() : pJog1(NULL), pJog2(NULL), proxFase(0), fase1(), fase2(), menu_principal(), multiplayer(0) {
+	randomizar();
+	pJog1 = new Jogador();
+	pJog2 = new Jogador(1);
+	//GC.setJog1(pJog1);
+}
+
+Jogo::~Jogo() {
+	ofstream arquivoFisico("save.txt");
+
+	/*cout << "Caminho do save: "
+		<< filesystem::absolute("save.txt") << endl;*/
+
+	if (arquivoFisico.is_open()) {
+
+		cout << "Salvando... " << endl;
+
+		fase1.salvarEntidades(arquivoFisico);
+		fase2.salvarEntidades(arquivoFisico);
+
+		arquivoFisico.close();
+
+		cout << "Jogo salvo! " << endl;
+	}
+	else {
+		cout << "Erro ao abrir o arquivo de save para escrita." << endl;
+	}
+
+	cout << "Limpando jogo... " << endl;
+
+	fase1.limpar();
+	fase2.limpar();
+
+    cout << "Deletando Jogador de Id: " << pJog1->getId() << " Tipo: " << typeid(*pJog1).name() << "... ";
+    delete pJog1;
+    cout << "Deletado" << endl;
+    pJog1=NULL;
+
+    cout << "Deletando Jogador de Id: " << pJog2->getId() << " Tipo: " << typeid(*pJog2).name() << "... ";
+    delete pJog2;
+    cout << "Deletado" << endl;
+    pJog2=NULL;
+}
+
+Jogador* Jogo::getJogador1() {
+	return pJog1;
+}
+Jogador* Jogo::getJogador2() {
+	return pJog2;
+}
+void Jogo::executar() {
+
+	Ente::setGG(&GG);
+
+	menu_principal.setJogo(this);
+
+	//fase1.setJogadores(pJog1);
+	fase1.setJogo(this);
+
+	//fase2.setJogadores(pJog1);
+	fase2.setJogo(this);
+
+	while(GG.isOpen() && proxFase >=0){
+        switch (proxFase) {
+            case 0:
+                menu_principal.executar();
+                printf("Saindo do menu, para fase %d\n",proxFase);
+                break;
+            case 1:
+                Entidade::setFase(1);
+                if(multiplayer==false){
+                    fase1.setJogadores(pJog1,NULL);
+                } else {
+                    fase1.setJogadores(pJog1,pJog2);
+                }
+                fase1.executar();
+                break;
+            case 2:
+                Entidade::setFase(2);
+                if(multiplayer==false){
+                    fase2.setJogadores(pJog1,NULL);
+                } else {
+                    fase2.setJogadores(pJog1,pJog2);
+                }
+                fase2.executar();
+                break;
+        }
+	}
+}
---
+++ Backups-codigos/Jogo_V26_Leonardo/main.cpp
@@ -0,0 +1,34 @@
+//Versão 26
+//terminei de renomear chefão como dragão
+//textura da lama trocada
+//textura da plataforma ajustada para não ficar esticada
+//plataformas agora têm altura e largura como múltiplos de 8, para ficar alinhado com a textura
+//textura dos ratos troca de sentido corretamente
+//aumentada altura do jogador de 50 para 55 (para ficar mais coerente com a textura)
+//trocada textura das águias (e a textura foi posicionada corretamente na hitbox)
+//adicionada textura do dragão (com uma versão diferente para quando leva dano)
+//hitbox do dragão foi ajustada
+//adicionada textura do projétil
+
+#include "Principal.h"
+
+int main()
+{
+    Jogo jogo;
+    jogo.executar();
+
+    return 0;
+}
+/*PARA FAZER:
+mudar a textura do menu. do jeito atual ele começa sem textura nenhuma, e quando sai de uma fase o menu fica com a textura daquela fase.
+
+mover checagem de posição pela função gravitar() para o GC?
+
+crash quando fica aberto por muito tempo?
+malloc: mismatching next->prev_size (unsorted)
+Aborted (core dumped)
+SAVES!
+
+revisar código para remover funções desnecessárias, remover códigos antigos comentados, etc
+
+*/
