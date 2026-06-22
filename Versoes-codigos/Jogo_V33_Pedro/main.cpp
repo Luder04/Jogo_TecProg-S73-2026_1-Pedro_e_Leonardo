dbaeac589fb2c9b46fcbd01a1d714bb519bbbf1e
Luder04 <ludergamespmm@gmail.com>
2026-06-21 23:28:22 -0300

    Add files via upload

---
+++ Versoes-codigos/Jogo_V33_Pedro/main.cpp
@@ -0,0 +1,23 @@
+// Versão 33
+
+#include "Principal.h"
+
+int main()
+{
+    Jogo jogo;
+    jogo.executar();
+
+    return 0;
+}
+/*PARA FAZER:
+ratos (e raramente águias) vão para cima da plataforma quando colidem com ela por baixo. será que deve ficar assim?
+
+mudar a textura do menu. do jeito atual ele começa sem textura nenhuma, e quando sai de uma fase o menu fica com a textura daquela fase.
+
+crash quando fica aberto por muito tempo?
+malloc: mismatching next->prev_size (unsorted)
+Aborted (core dumped)
+
+revisar código para remover funções desnecessárias, remover códigos antigos comentados, etc
+
+*/
