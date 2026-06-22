299bd42d38c2139d7f33b22e9d197cbdc6aae6be
Luder04 <ludergamespmm@gmail.com>
2026-06-22 01:01:04 -0300

    Add files via upload

---
+++ Backups-codigos/Jogo_V33.1_PedroLeonardo/Principal.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Gerenciadores.h"
+#include "stdio.h"
+#include "Personagens.h"
+#include "Fases.h"
+#include "Ente.h"
+#include "menu.h"
+
+using namespace Fases;
+using namespace Personagens;
+
+class Jogo {
+private:
+
+	Jogador* pJog1;
+	Jogador* pJog2;
+	Gerenciadores::GerenciadorGrafico GG;
+	//Gerenciadores::GerenciadorColisoes GC;
+
+public:
+	static int proxFase;
+	bool multiplayer;
+	FasePrimeira fase1;
+    FaseSegunda fase2;
+
+    Menu menu_principal;
+
+public:
+	Jogo();
+	~Jogo();
+
+	Jogador* getJogador1();
+    Jogador* getJogador2();
+	void executar();
+	void recuperar();
+	void recuperaRank();
+	void randomizar();
+};
